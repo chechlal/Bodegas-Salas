d3bb35fd8b72b1d7546429489e34fe03e7f0b78c
chechlal <rsfdez286@gmail.com>
2025-12-07 06:01:12 -0300

    backend/urls.py: Forzar a servir imagenes media en render linea 23 a 25

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.static import serve
 from companies.views import MyTokenObtainPairView
 
 urlpatterns = [
@@ -13,12 +14,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # 2. Rutas de tu API original (Productos, Marcas, Categorias)
-    # Esto busca el archivo urls.py dentro de tu carpeta 'api'
     path('api/', include('api.urls')),
 
     # 3. Rutas del Login Nuevo (Seguridad JWT)
-    # Estas son las URLs que tu Frontend "nuevo" usa para loguearse
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
